658a8be589373ab59d54a51d3761a4ceaadc4d23
Michael Kelly <mkelly@mozilla.com>
2012-12-08 17:07:33 -0500

    Convert test_set to a test generator.

--- snoboy/tests/test_set.py
+++ snoboy/tests/test_set.py
@@ -1,13 +1,12 @@
-import unittest
+from nose.tools import eq_
 
 from snoboy import cpu, instructions
 
 
-class Set(unittest.TestCase):
-    def testSet(self):
-        bit = 0
-        for opcode in range(0xC0, 0xFF, 8):
-            cpu.registers.B = 0
-            instructions.set(opcode)
-            self.assertEquals(cpu.registers.B, 1 << bit)
-            bit = bit + 1
+def test_set():
+    bit = 0
+    for opcode in range(0xC0, 0xFF, 8):
+        cpu.registers.B = 0
+        instructions.set(opcode)
+        yield eq_, cpu.registers.B, 1 << bit
+        bit = bit + 1
